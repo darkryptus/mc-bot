f7dc76fcbba366fabdcf3fa305ccd9658b0c4c77
Dark <darkryptus@gmail.com>
2026-03-30 13:40:44 +0530

    Update main.js

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ const MAX_INDEX = 100
 const ROTATION_INTERVAL = 2 * 60 * 60 * 1000 // ✅ 5 MINUTES
 
 function getUsername () {
-  return `bsdk${nameIndex}`
+  return `tet${nameIndex}`
 }
 
 function incrementUsername () {
